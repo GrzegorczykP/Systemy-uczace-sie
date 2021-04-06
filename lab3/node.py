ee8befe17ca50023bb2612fd74b745f3e4ee22dd
GrzegorczykP <pawlo6697@gmail.com>
2021-04-06 15:42:29 +0200

    Printing decision tree

--- lab3/node.py
+++ lab3/node.py
@@ -4,3 +4,19 @@ class Node:
         self.decision_class = None
         self.test = None
         self.input_value = None
+
+    def __str__(self):
+        if self.decision_class is not None:
+            return f'{self.input_value} -> Klasa decyzyjna "{self.decision_class}"\n'
+        else:
+            text = ''
+            if self.input_value is None:
+                text += f'KORZEŃ -> Węzeł (a{self.test})\n'
+            else:
+                text += f'{self.input_value} -> Węzeł (a{self.test})\n'
+
+            for child in self.children:
+                t = str(child)
+                text += '\n'.join(map(lambda x: '\t' + x, t.strip().split('\n'))) + '\n'
+
+            return text
